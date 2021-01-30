5d323b7b0581d390b990ae2d69ccea2a91d9dded
Kemosabe2911 <stevinprince29@gmail.com>
2021-01-30 10:01:08 +0530

    Update Display

--- jokes-app/src/components/DisplayJokes.js
+++ jokes-app/src/components/DisplayJokes.js
@@ -1,20 +1,25 @@
 import React from 'react'
 
 const DisplayJokes = ({result}) =>{
-    const jokes=[];
+    var jokes=[];
+    var items=[];
+
     {
         result.map(res => jokes.push(res));
     }
-    console.log(jokes)
+    console.log(jokes);
+    for( var i=0; i< jokes.length;i++){
+        items= items.concat(jokes[i]);
+    }
+    console.log(items);
     return(
         <div className="output">
             <h1>Hello</h1>
         <ul>
-            {jokes.map( (joke) =>{
-                joke.map((data) =>{
-                    <li>{data}</li>
-                })
+            {items.map((data) =>{
+                <li>{data}</li>
             })}
+            <li>Hello</li>
         </ul>
         </div>
     )
