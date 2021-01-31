1f50707877ec8e564d7d8dc70697e61fc3a1ed57
Kemosabe2911 <stevinprince29@gmail.com>
2021-01-31 11:58:33 +0530

    Add theme toggler

--- jokes-app/src/App.js
+++ jokes-app/src/App.js
@@ -5,6 +5,34 @@ import DisplayJokes from './components/DisplayJokes'
 
 function App() {
   const [result,setResult] = useState([]);
+  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+  
+  React.useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
+    const userPrefersDark = getPrefColorScheme();
+    // if mode was saved --> dark / light
+    if (isReturningUser) {
+      return savedMode;
+      // if preferred color scheme is dark --> dark
+    } else if (userPrefersDark) {
+      return true;
+      // otherwise --> light
+    } else {
+      return false;
+    }
+    // return savedMode || false;
+  }
+
+  function getPrefColorScheme() {
+    if (!window.matchMedia) return;
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
 
   const getrandomJokes = () =>{
     let temp =[];
@@ -14,7 +42,7 @@ function App() {
         let data = res.data.value;
         temp.push(data);
       })
-      console.log('Hello');
+      //console.log('Hello');
     }
     console.log(temp);
     setResult([...result,temp]);
@@ -22,24 +50,37 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className= {darkMode ? "dark-mode" : "light-mode"} >
+      <div className="App">
       <header className="App-header">
         Jokes App
       </header>
+      <nav>
+        <div className="toggle-container">
+          <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+          <span className="toggle">
+            <input
+              checked={darkMode}
+              onChange={() => setDarkMode(prevMode => !prevMode)}
+              id="checkbox"
+              className="checkbox"
+              type="checkbox"
+            />
+            <label htmlFor="checkbox" />
+          </span>
+          <span style={{ color: darkMode ? "slateblue" : "grey" }}>☾</span>
+          {/* <button onClick={() => setDarkMode(prevMode => !prevMode)}>
+          Toggle
+        </button> */}
+        </div>
+      </nav>
       <div className="button">
         <button className="btn" onClick={getrandomJokes}>Get Jokes</button>
         <div className="list">
           <DisplayJokes result={result} />
         </div>
       </div>
-      <div className="theme-switch-wrapper">
-        <label className="theme-switch-handler">
-          <div className="switch-path">
-            <div className="switch-handle">
-            </div>
-          </div>
-        </label>
-      </div>
+    </div>
     </div>
   );
 }
